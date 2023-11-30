cedd7f9c82dcc3d3e3a335b4ea3c201035a10f88
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-11-30 23:37:07 +0000

    Added nvim config files.

---
+++ nvim/lua/rensoguilalas/core/options.lua
@@ -0,0 +1,41 @@
+local opt = vim.opt -- for consiseness
+
+-- highlighting the trailing whitespace
+vim.cmd([[ highlight ExtraWhitespace ctermbg=red guibg=red ]])
+vim.cmd([[ match ExtraWhitespace /\s\+$/ ]])
+
+-- line numbers
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
---
+++ nvim/lua/rensoguilalas/plugins/lsp/mason.lua
@@ -0,0 +1,56 @@
+-- import mason plugin safely
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+	return
+end
+
+-- import mason-lspconfig plugin safely
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+	return
+end
+
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+	return
+end
+
+-- enable mason
+mason.setup()
+
+mason_lspconfig.setup({
+	-- list of servers for mason to install
+	ensure_installed = {
+		-- configured
+		"tsserver",
+		"html",
+		"cssls",
+		"tailwindcss",
+		"lua_ls",
+		"emmet_ls",
+		"hls", -- Haskell
+		"clangd", -- C
+		"sqlls", -- SQL
+		"pylsp", -- Python
+		"marksman", -- Markdown
+		"kotlin_language_server", -- Kotlin
+		"jdtls", -- Java
+
+		-- Not configured
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
+})
+
+mason_null_ls.setup({
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"prettier", -- ts/js formatter
+		"stylua", -- lua formatter
+		"eslint_d", -- ts/js linter
+		"ast-grep", -- ast_grep (keywords: c, c++, rust, go, java, python, c#, javascript, jsx, typescript, html, css, kotlin, dart, lua)
+	},
+	-- auto-install configured formatters & linters (with null-ls)
+	automatic_installation = true,
+})
